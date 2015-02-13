81184e084d88afbf2c4a8ea3153e5bfc2b7046ea
Riccardo Miccini <rimiccini@yahoo.it>
2015-02-13 07:15:45 +0100

    author admin

--- ecvitae/apps/cvitae/admin.py
+++ ecvitae/apps/cvitae/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from ecvitae.apps.cvitae.models import Author
 
-# Register your models here.
+
+class AuthorInline(admin.StackedInline):
+    model = Author
+    can_delete = False
+    verbose_name_plural = 'authors'
+
+# Define a new User admin
+class UserAdmin(UserAdmin):
+    inlines = (AuthorInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
