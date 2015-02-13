426c7af446fe693e9b7d729ca0cf59d7ab133229
Riccardo Miccini <rimiccini@yahoo.it>
2015-02-13 06:37:59 +0100

    added models

--- ecvitae/apps/cvitae/models.py
+++ ecvitae/apps/cvitae/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Employer(models.Model):
+	name		= models.CharField(max_length=50)
+	loc			= models.CharField(max_length=50)
+	business	= models.CharField(max_length=50, blank=True)
+	
+	def __unicode__(self):
+		return self.name
+
+
+class Author(models.Model):
+	user		= models.OneToOneField(User)
+	firstname	= models.CharField(max_length=50)
+	surname		= models.CharField(max_length=50)
+	address		= models.TextField()
+	birthday	= models.DateField()
+	email2		= models.EmailField(blank=True)
+	phone		= models.CharField(max_length=20, blank=True)
+	
+	def __unicode__(self):
+		return self.name
+
+	
+class Job(models.Model):
+	position	= models.CharField(max_length=30)
+	author		= models.ForeignKey(Author)
+	employer	= models.ForeignKey(Employer)
+	descr		= models.TextField()
+	start_date	= models.DateField()
+	end_date	= models.DateField()
+	
+	def __unicode__(self):
+		return self.position
+
+
+class CV(models.Model):
+	author	= models.ForeignKey(Author)
+	descr	= models.TextField(blank=True)
+	added	= models.DateTimeField(auto_now_add=True)
+	updated	= models.DateTimeField(auto_now=True)
+	
+	def __unicode__(self):
+		return self.descr
+
+
+
+	
+	
+	
