d9a6584cc136c1a553aa8c90c4705682fe42944d
Riccardo Miccini <rimiccini@yahoo.it>
2015-02-14 14:15:32 +0100

    sitetree support

--- ecvitae/settings.py
+++ ecvitae/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'ecvitae.apps.cvitae',
     'ecvitae.mytemplates',
+    'sitetree',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -47,6 +48,17 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
 )
 
 ROOT_URLCONF = 'ecvitae.urls'
