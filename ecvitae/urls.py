e319183016ce46a448ad68c8a2c233005723f0a7
Riccardo Miccini <rimiccini@yahoo.it>
2015-02-14 03:39:23 +0100

    fixed urls.py

--- ecvitae/urls.py
+++ ecvitae/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .views import home, home_files
 
 urlpatterns = patterns('',
-	url(r'^$', home, name='home')
+	url(r'^$', home, name='home'),
 	url(r'^(?P<filename>(robots.txt)|(humans.txt))$', home_files, name='home-files'),
 
 	url(r'^admin/', include(admin.site.urls)),
